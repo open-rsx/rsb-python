2328fed54c0abf3b6cf4d53f4437814409439be1
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2010-08-26 15:48:13 +0000

    OrderedQueueDispatcherPool ported from C++
    fixes #93

--- test/utiltest.py
+++ test/utiltest.py
@@ -18,6 +18,11 @@
 import unittest
 
 import rsb.util
+from threading import Condition
+import time
+import random
+from rsb.util import OrderedQueueDispatcherPool
+import logging
 
 class EnumValueTest(unittest.TestCase):
     
@@ -45,3 +50,128 @@ class EnumTest(unittest.TestCase):
         self.assertEqual(rsb.util.Enum.EnumValue("C"), e.C)
         
         self.assertEqual("Enum e: A, B, C", str(e))
+        
+class OrderedQueueDispatcherPoolTest(unittest.TestCase):
+    
+    def setUp(self):
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+    
+    class StubReciever(object):
+        
+        nextReceiverNum = 1
+        @classmethod
+        def nextNumber(cls):
+            num = cls.nextReceiverNum
+            cls.nextReceiverNum = cls.nextReceiverNum + 1
+            return num
+        
+        def __init__(self):
+            self.receiverNum = self.nextNumber()
+            self.condition = Condition()
+            self.messages = []
+            
+        def __str__(self):
+            return "StubReceiver%d" % self.receiverNum
+            
+    def deliver(self, receiver, message):
+        
+        with receiver.condition:
+            
+            time.sleep(random.random() * 0.05 * float((receiver.receiverNum % 10)))
+            
+            receiver.messages.append(message)
+            receiver.condition.notify_all()
+            
+    def testProcessing(self):
+        
+        numMessages = 100
+        pool = OrderedQueueDispatcherPool(4, self.deliver)
+        
+        numReceivers = 11
+        receivers = []
+        for i in range(numReceivers):
+            r = self.StubReciever()
+            pool.registerReceiver(r)
+            receivers.append(r)
+            
+        pool.start()
+        try:
+            pool.start()
+            self.fail("Starting an already running pool must not be possible")
+        except RuntimeError:
+            pass
+        
+        # start jobs
+        for i in range(numMessages):
+            pool.push(i)
+            
+        # wait for processing
+        for receiver in receivers:
+            with receiver.condition:
+                while len(receiver.messages) < numMessages:
+                    receiver.condition.wait()
+                    
+        pool.stop()
+        
+        # check receivers
+        for receiver in receivers:
+            
+            self.assertEqual(numMessages, len(receiver.messages))
+            
+            for i in range(numMessages):
+                self.assertEqual(i, receiver.messages[i])
+        
+    class RejectFilter(object):
+        
+        def __init__(self):
+            self.rejectCalls = 0
+            self.condition = Condition()
+            
+        def __call__(self, receiver, message):
+            with self.condition:
+                self.rejectCalls = self.rejectCalls + 1
+                self.condition.notify_all();
+                return False
+                
+    def testFilterExecution(self):
+        
+        filter = self.RejectFilter()
+        pool = OrderedQueueDispatcherPool(2, self.deliver, filter)
+        
+        receiver = self.StubReciever()
+        pool.registerReceiver(receiver)
+        
+        pool.start()
+        
+        numMessages = 10;
+        for i in range(numMessages):
+            pool.push(i)
+            
+        # wait for filtering
+        with filter.condition:
+            while filter.rejectCalls < numMessages:
+                filter.condition.wait()
+        
+        pool.stop()
+        
+        self.assertEqual(numMessages, filter.rejectCalls)
+        
+    def testUnregister(self):
+        
+        pool = OrderedQueueDispatcherPool(2, self.deliver)
+        
+        pool.start()
+        
+        receiver = self.StubReciever()
+        pool.registerReceiver(receiver)
+        self.assertTrue(pool.unregisterReceiver(receiver))
+        self.assertFalse(pool.unregisterReceiver(receiver))
+        
+        pool.push(42)
+        
+        pool.stop()
+        
+        time.sleep(0.1)
+        
+        self.assertEqual(0, len(receiver.messages))
