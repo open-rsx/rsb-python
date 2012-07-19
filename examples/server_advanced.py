f47963dd24b3886d3cd66cbf623a37dccaa937fc
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-07-19 19:45:08 +0000

    Added advanced server usage in examples/server_advanced.py * examples/server_advanced.py: new file; demonstrates some of the   advanced server usage patterns which have been removed from the   simple example in examples/server.py

---
+++ examples/server_advanced.py
@@ -0,0 +1,61 @@
+# ============================================================
+#
+# Copyright (C) 2011, 2012 Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+#
+# This file may be licensed under the terms of the
+# GNU Lesser General Public License Version 3 (the ``LGPL''),
+# or (at your option) any later version.
+#
+# Software distributed under the License is distributed
+# on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+# express or implied. See the LGPL for the specific language
+# governing rights and limitations.
+#
+# You should have received a copy of the LGPL along with this
+# program. If not, go to http://www.gnu.org/licenses/lgpl.html
+# or write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# The development of this software was supported by:
+#   CoR-Lab, Research Institute for Cognition and Robotics
+#     Bielefeld University
+#
+# ============================================================
+
+import logging
+from time import sleep
+
+from rsb import Scope, createServer
+
+if __name__ == '__main__':
+    # Pacify logger.
+    logging.basicConfig()
+
+    # Create a LocalServer object that exposes its methods under the
+    # scope /example/server.
+    server = createServer(Scope('/example/server'))
+
+    # Add a method to the server.
+    server.addMethod('echo', lambda x: x, str, str)
+
+    # It is also possible to create a LocalServer with a given set of
+    # methods. This construction avoids adding the methods
+    # individually.
+    server = createServer(Scope('/example/server'),
+                          methods = [ ('echo2', lambda x: x, str, str) ])
+
+    # Finally, a LocalServer can be created by exposing some or all
+    # methods of an ordinary Python object.
+    class MyObject:
+        def echo3(self, arg):
+            return arg
+
+    server = createServer(Scope('/example/server'),
+                          object = MyObject(),
+                          expose = [ ('echo3', str, str) ])
+
+    # Note: the code above creates three servers, each of which
+    # provides one method on the scope /example/server
+
+    # Wait for method calls by clients.
+    sleep(100)
