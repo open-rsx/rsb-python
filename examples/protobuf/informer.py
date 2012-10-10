5a1256385dac5de29bb784c9d91746564558a91c
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-10-10 21:04:53 +0200

    Simplified and reorganized examples/protobuf/*.py
    refs #1196
    
    * examples/protobuf/registration.py: new file; illustrates
      registration of converters
    * examples/protobuf/informer.py: cleaned up; shortened comments
      regarding registration; added sphinx marks
    * examples/protobuf/listener.py: likewise

--- examples/protobuf/informer.py
+++ examples/protobuf/informer.py
@@ -22,18 +22,13 @@
 #
 # ============================================================
 
+# mark-start::body
 import logging
 
 import rsb
+import rsb.converter
 
-from rsb.converter import registerGlobalConverter, getGlobalConverterMap, ProtocolBufferConverter
-
-# Load the generated protocol buffer data holder class from the
-# current directory. This would look different if the protocol buffer
-# code generation was properly integrated in a build process.
-#
-# See the comment in SimpleImage.proto for how to manually perform the
-# code generation.
+# See ./registration.py.
 import sys
 sys.path.append('.')
 from SimpleImage_pb2 import SimpleImage
@@ -42,37 +37,24 @@ if __name__ == '__main__':
     # Pacify logger.
     logging.basicConfig()
 
-    # Register a protocol buffer converter for SimpleImage:
-    # The generated data holder class is
-    #   SimepleImage_pb2.SimpleImage
-    # The protocol buffer message is called
-    #   .tutorial.protobuf_converter.SimpleImage
-    converter = ProtocolBufferConverter(messageClass = SimpleImage)
-    registerGlobalConverter(converter)
+    # See ./registration.py.
+    converter = rsb.converter.ProtocolBufferConverter(messageClass = SimpleImage)
+    rsb.converter.registerGlobalConverter(converter)
 
-    print("Registered converter %s" % converter)
-    print("Registered converters:\n%s " % getGlobalConverterMap(bytearray))
-
-    # After registering one or more converters, it is currently
-    # necessary to replace the default participant configuration with
-    # a fresh one which takes into account the newly registered
-    # converters.
-    #
-    # This will hopefully become unnecessary in future RSB versions.
     rsb.__defaultParticipantConfig = rsb.ParticipantConfig.fromDefaultSources()
 
     # Create an informer that will send the events carrying protocol
     # buffer payloads. See the informer.py example for a more detailed
     # explanation of informer creation.
-    informer = rsb.createInformer(rsb.Scope("/tutorial/converter"),
+    informer = rsb.createInformer("/example/converter",
                                   dataType = SimpleImage)
 
-    print("Informer setup finished")
-
-    image = SimpleImage()
-    image.width  = 100
-    image.height = 100
-    image.data   = str('bla')
-    informer.publishData(image)
-
-    print("Send data, exiting")
+    try:
+        image = SimpleImage()
+        image.width  = 100
+        image.height = 100
+        image.data   = str('bla')
+        informer.publishData(image)
+    finally:
+        informer.deactivate()
+# mark-end::body
