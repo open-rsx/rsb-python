748c075356b18f5079fca7e730c7c08a1637e5f5
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2013-02-08 11:41:34 +0100

    Make converter selection really dynamic in case of Server methods.
    * The reply event was attached with the declared data type for the method instead of the one of the data type.
    * Added a unit test for this case
    
    refs #1404

--- rsb/patterns/__init__.py
+++ rsb/patterns/__init__.py
@@ -288,7 +288,7 @@ class LocalMethod (Method):
                 result = self._func()
             else:
                 result = self._func(request.data)
-            resultType = self.replyType
+            resultType = type(result)
         except Exception, e:
             isError                 = True
             userInfos['rsb:error?'] = '1'
@@ -306,7 +306,7 @@ class LocalMethod (Method):
             # created with type 'object' to enable throwing exceptions
             if not isError and not isinstance(result, self.replyType):
                 raise ValueError("The result '%s' (of type %s) of method %s does not match the method's declared return type %s."
-                                 % (result, type(result), self.name, self.replyType))
+                                 % (result, resultType, self.name, self.replyType))
             reply = rsb.Event(scope     = self.informer.scope,
                               method    = 'REPLY',
                               data      = result,
