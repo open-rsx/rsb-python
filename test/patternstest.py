d2b1a1afd0b47d114c8e5f3019e63136ec22aefe
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2013-04-23 14:05:12 +0200

    Let the user specify whether an RPC method can be called in parallel or not.
    * Add a flag to addMethod for LocalServer to specify whether async
    behavior is desired.
    * Based on flag, chose either the usual configuration of the listener or
    use the new FullyParallelEventReceivingStrategy.
    * Add an integration-like unit test for this behavior.

--- test/patternstest.py
+++ test/patternstest.py
@@ -25,44 +25,46 @@
 import unittest
 import rsb
 from rsb import ParticipantConfig
+import time
+from threading import Condition
 
 class LocalServerTest (unittest.TestCase):
     def testConstruction(self):
-
+  
         # Test creating a server without methods
         server = rsb.createServer('/some/scope', config=ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"}))
         self.assertEqual(server.methods, [])
         server.deactivate()
-
+  
         server = rsb.createServer(rsb.Scope('/some/scope'), config=ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"}))
         self.assertEqual(server.methods, [])
         server.deactivate()
-
+  
         # Test creating a server with directly specified methods
         server = rsb.createServer(rsb.Scope('/some/scope'),
                                   methods = [ ('foo', lambda x: x, str, str) ], config=ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"}))
         self.assertEqual([ m.name for m in server.methods ], [ 'foo' ])
         server.deactivate()
-
+  
         # Test creating a server that exposes method of an existing
         # object
         class SomeClass (object):
             def bar(x):
                 pass
-
+  
         someObject = SomeClass()
         server = rsb.createServer(rsb.Scope('/some/scope'),
                                   object = someObject,
                                   expose = [ ('bar', str, None) ],
                                   config=ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"}))
         self.assertEqual([ m.name for m in server.methods ], [ 'bar' ])
-
+  
         # Cannot supply expose without object
         self.assertRaises(ValueError,
                           rsb.createServer,
                           '/some/scope',
                           expose  = [ ('bar', str, None) ])
-
+  
         # Cannot supply these simultaneously
         self.assertRaises(ValueError,
                           rsb.createServer,
@@ -74,41 +76,84 @@ class LocalServerTest (unittest.TestCase):
 
 class RoundTripTest (unittest.TestCase):
     def testRoundTrip(self):
-
+  
         config = ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"})
-
+  
         localServer = rsb.createServer(
             '/roundtrip',
             methods = [ ('addone', lambda x: long(x + 1), long, long) ], config=config)
-
+  
         remoteServer = rsb.createRemoteServer('/roundtrip', config)
-
+  
         # Call synchronously
         self.assertEqual(map(remoteServer.addone, range(100)),
                          range(1, 101))
-
+  
         # Call asynchronously
         self.assertEqual(map(lambda x: x.get(),
                              map(remoteServer.addone.async, range(100))),
                          range(1, 101))
-
+  
         localServer.deactivate()
         remoteServer.deactivate()
-        
+          
     def testVoidMethods(self):
-        
+          
         config = ParticipantConfig.fromDict({"transport.socket.enabled" : "1"})
-        
+          
         localServer = rsb.createServer('/void', config=config)
         def nothing(e):
             pass
         localServer.addMethod("nothing", nothing, str)
-
+  
         remoteServer = rsb.createRemoteServer('/void', config)
-        
+          
         future = remoteServer.nothing.async("test")
         try:
             future.get(1)
+        finally:
+            localServer.deactivate()
+            remoteServer.deactivate()
+            
+    def testParallelCallOfOneMethod(self):
+
+        config = ParticipantConfig.fromDict({"transport.inprocess.enabled" : "1"})
+        
+        class Counter(object):
+            def __init__(self):
+                self.value = 0
+        maxParallelCalls = Counter()
+        currentCalls = []
+        callLock = Condition()
+        
+        try:
+            localServer = rsb.createServer('/takesometime', config=config)
+            def takeSomeTime(e):
+                with callLock:
+                    currentCalls.append(e)
+                    maxParallelCalls.value = max(maxParallelCalls.value, len(currentCalls))
+                    callLock.notifyAll()
+                time.sleep(2)
+                with callLock:
+                    currentCalls.remove(e)
+                    callLock.notifyAll()
+            localServer.addMethod("takeSomeTime", takeSomeTime, str, async=True)
+    
+            remoteServer = rsb.createRemoteServer('/takesometime', config)
+    
+            remoteServer.takeSomeTime.async("test1")
+            remoteServer.takeSomeTime.async("test2")
+            remoteServer.takeSomeTime.async("test3")
+    
+            numCalled = 0
+            with callLock:
+                while maxParallelCalls.value < 3 and numCalled < 5:
+                    numCalled = numCalled + 1
+                    callLock.wait()
+                if numCalled == 5:
+                    self.fail("Impossible to be called in parallel again")
+                else:
+                    self.assertEqual(3, maxParallelCalls.value)
         finally:
             localServer.deactivate()
             remoteServer.deactivate()
@@ -118,3 +163,4 @@ def suite():
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(LocalServerTest))
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(RoundTripTest))
     return suite
+    
