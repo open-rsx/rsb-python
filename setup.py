777d9b48415866c9c6e7526a5e754c93b418951a
Johannes Wienke <jwienke@techfak.uni-bielefeld.de>
2013-07-03 14:51:34 +0200

    Add support for executing pylint.
    * setup.cfg: added configuration options for pylint. We now need to take care that this file will not be committed with local changes like the protocolroot.
    * setup.py: declare dependency to setuptools lint command

--- setup.py
+++ setup.py
@@ -492,7 +492,7 @@ setup(name='rsb-python',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
 
-      setup_requires=["coverage", "epydoc", "unittest-xml-reporting", "protobuf"],
+      setup_requires=["coverage", "epydoc", "unittest-xml-reporting", "protobuf", "setuptools-lint"],
 
       extras_require={
         'spread-transport':  ["SpreadModule>=1.5spread4"],
