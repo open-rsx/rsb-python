7b0a7f6fa1585a8e900ef9ed3cde9a0a7a1a00d1
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-11-07 12:44:39 +0100

    Version bump to 0.16.0, refs #2560

--- setup.py
+++ setup.py
@@ -313,7 +313,7 @@ def generateVersionFile(version, commit):
                     '@VERSION@', version).replace(
                         '@COMMIT@', commit))
 
-version, commit = defineProjectVersion('0.15')
+version, commit = defineProjectVersion('0.16')
 print('This is version {version}-{commit}'.format(version=version,
                                                   commit=commit))
 generateVersionFile(version, commit)
